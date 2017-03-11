feafdb8e204aeb917457d1408945a4eac4175635
Raptor Dz <gregdzuricsko@gmail.com>
2017-03-10 22:24:36 -0600

    init

---
+++ Assets/scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	private Rigidbody rb;
+
+	public float speed;
+	//called on first frame script is active
+	void Start(){
+		rb = GetComponent<Rigidbody> ();
+	}
+
+	//not physics
+//	void Update(){
+//
+//	}
+
+	//Physics
+	void FixedUpdate(){
+		//Input used to get user input
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		//simplest rigidbody
+		Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+		rb.AddForce(movement * speed);// + does not work!
+	}
+		
+}
